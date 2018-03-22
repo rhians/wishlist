693b61720028481adda01c8fc81c5c800420d12e
rhians <rhian.farnworth@adidas-group.com>
2018-03-22 22:08:15 +0100

    Update apitest.js

--- client/src/components/apitest.js
+++ client/src/components/apitest.js
@@ -1,66 +1,44 @@
-//not in use - test attempt 
+//https://blog.hellojs.org/fetching-api-data-with-react-js-460fe8bbf8f2
 
 import React, { component } from 'react';
-import apitest from './styling/apitest.css';
+import apitest from '../styling/apitest.css';
 
-class Home extends React.Component {
+class apidata extends React.Component {
 
-constructor(){
-  super();
-  this.state = {};
+constructor(props) {
+  super(props);
+  this.state = { suggestion: [],
+  };
 }
 
 componentWillMount(){
-  // Called the first time the component is loaded right before the component is added to the page
-  var url = "https://www.adidas.co.uk/api/suggestions/${query}";
-  //i added in the JSX query - I think I need to define this somewhere else first though?
-  Request.get(url).then((response) => {
-    this.setState({
-      products: response
-    });
-  });
+  fetch('https://www.adidas.co.uk/api/suggestions/Stans%20Smith')
+  .then(results => {
+    return results.json();
+  }).then(data => {
+    let image = data.results.map((suggestion) => {
+      return(
+        <div key={pic.results}>
+        <img src={pic.results.medium} />
+        </div>
+      )
+    })
+    this.setState({suggestion: suggestion});
+    console.log("state", this.state.suggestion);
+  })
 }
 
-componentDidMount(){
-  // Called after the component has been rendered into the page
+render() {
+  return (
+    <ul>
+    //create containers
+    <div classname="container1">
+    <div className="container2">
+    {this.state.suggestion}
+    </div>
+    </div>
+    </ul>
+  )
 }
 
-componentWillReceiveProps(nextProps){
-  // Called when the props provided to the component are changed
-}
-
-componentWillUpdate(nextProps, nextState){
-  // Called when the props and/or state change
-}
-
-componentWillUnmount(){
-  // Called when the component is removed
-}
-
-updateSearch(){
-  this.search(this.refs.query.value);
-}
-
-render(){
-  var products = _.map(this.state.products, (products) => {
-    return <li>{products}</li>;
-  });
-  return <div>
-    <input ref="query" onChange={ (e) => { this.updateSearch(); } } type="text" />
-    <ul>{products}</ul>
-  </div>;
-}
-
-search(query = "stan smith"){
-  var url = `https://www.adidas.co.uk/api/suggestions/Stans%20Smith`;
-  Request.get(url).then((response) => {
-    this.setState({
-      products: response.body.Search,
-      total: response.body.totalResults
-    });
-  });
-}
-
-}
-
-export { Home as default };
+export default apidata;
